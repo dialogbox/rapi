fea63f45ad341df15db25c9e26857c6a5345391b
Jason Kim <dialogbox@gmail.com>
2018-08-22 07:43:01 +0900

    testing

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate actix;
 extern crate actix_web;
 extern crate futures;
 extern crate tokio;
+extern crate tokio_timer;
 
 pub mod rng_actor;
 pub mod rng_handlers;
--- src/rng_handlers.rs
+++ src/rng_handlers.rs
@@ -1,11 +1,13 @@
-use actix_web::{HttpRequest, Path, dev::Handler};
-use actix_web::FromRequest;
+use actix_web::{AsyncResponder, dev::Handler, HttpRequest, Path};
 use actix_web::error::Result;
-
-
-use xorshift::{Rng, Xorshift1024};
-use std::{thread, time};
+use actix_web::FromRequest;
+use actix_web::FutureResponse;
+use futures::Future;
+use futures::future::ok;
 use std::cell::RefCell;
+use std::time;
+use tokio_timer::sleep;
+use xorshift::{Rng, Xorshift1024};
 
 pub struct RandHandler (pub RefCell<Xorshift1024>);
 
@@ -20,14 +22,16 @@ impl<S> Handler<S> for RandHandler {
 pub struct SlowRandHandler (pub RefCell<Xorshift1024>);
 
 impl<S> Handler<S> for SlowRandHandler {
-    type Result = Result<String>;
+    type Result = Box<Future<Item = String, Error = tokio_timer::Error>>;
 
     fn handle(&self, req: &HttpRequest<S>) -> Self::Result {
-        let params = *Path::<(u64)>::extract(req)?;
+        let params = *Path::<(u64)>::extract(req).unwrap();
         if params > 0 {
-            thread::sleep(time::Duration::from_millis(params));
+            sleep(time::Duration::from_millis(params))
+                .and_then(|()| ok(format!("{}", self.0.borrow_mut().next_u64())))
+                .responder()
+        } else {
+            ok(format!("{}", self.0.borrow_mut().next_u64()))
         }
-
-        Ok(format!("{}", self.0.borrow_mut().next_u64()))
     }
 }
